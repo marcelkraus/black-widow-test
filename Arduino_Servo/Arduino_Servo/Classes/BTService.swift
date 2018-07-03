e26645bec15b92e9191d26c7069e6bcf614653d2
Marcel Kraus <mail@marcelkraus.de>
2018-07-03 16:47:55 +0200

    Make whitespace look great again

--- Arduino_Servo/Arduino_Servo/Classes/BTService.swift
+++ Arduino_Servo/Arduino_Servo/Classes/BTService.swift
@@ -15,93 +15,92 @@ let PositionCharUUID = CBUUID(string: "F38A2C23-BC54-40FC-BED0-60EDDA139F47")
 let BLEServiceChangedStatusNotification = "kBLEServiceChangedStatusNotification"
 
 class BTService: NSObject, CBPeripheralDelegate {
-  var peripheral: CBPeripheral?
-  var positionCharacteristic: CBCharacteristic?
-  
-  init(initWithPeripheral peripheral: CBPeripheral) {
-    super.init()
-    
-    self.peripheral = peripheral
-    self.peripheral?.delegate = self
-  }
-  
-  deinit {
-    self.reset()
-  }
-  
-  func startDiscoveringServices() {
-    self.peripheral?.discoverServices([BLEServiceUUID])
-  }
-  
-  func reset() {
-    if peripheral != nil {
-      peripheral = nil
+    var peripheral: CBPeripheral?
+    var positionCharacteristic: CBCharacteristic?
+
+    init(initWithPeripheral peripheral: CBPeripheral) {
+        super.init()
+
+        self.peripheral = peripheral
+        self.peripheral?.delegate = self
     }
-    
-    // Deallocating therefore send notification
-    self.sendBTServiceNotificationWithIsBluetoothConnected(false)
-  }
-  
-  // Mark: - CBPeripheralDelegate
-  
-  func peripheral(_ peripheral: CBPeripheral, didDiscoverServices error: Error?) {
-    let uuidsForBTService: [CBUUID] = [PositionCharUUID]
-    
-    if (peripheral != self.peripheral) {
-      // Wrong Peripheral
-      return
+
+    deinit {
+        self.reset()
     }
-    
-    if (error != nil) {
-      return
+
+    func startDiscoveringServices() {
+        self.peripheral?.discoverServices([BLEServiceUUID])
     }
-    
-    if ((peripheral.services == nil) || (peripheral.services!.count == 0)) {
-      // No Services
-      return
-    }
-    
-    for service in peripheral.services! {
-      if service.uuid == BLEServiceUUID {
-        peripheral.discoverCharacteristics(uuidsForBTService, for: service)
-      }
-    }
-  }
-  
-  func peripheral(_ peripheral: CBPeripheral, didDiscoverCharacteristicsFor service: CBService, error: Error?) {
-    if (peripheral != self.peripheral) {
-      // Wrong Peripheral
-      return
-    }
-    
-    if (error != nil) {
-      return
-    }
-    
-    if let characteristics = service.characteristics {
-      for characteristic in characteristics {
-        if characteristic.uuid == PositionCharUUID {
-          self.positionCharacteristic = (characteristic)
-          peripheral.setNotifyValue(true, for: characteristic)
-          
-          // Send notification that Bluetooth is connected and all required characteristics are discovered
-          self.sendBTServiceNotificationWithIsBluetoothConnected(true)
+
+    func reset() {
+        if peripheral != nil {
+            peripheral = nil
         }
-      }
+
+        // Deallocating therefore send notification
+        self.sendBTServiceNotificationWithIsBluetoothConnected(false)
     }
-  }
-  
-  // Mark: - Private
-  
-  func writePosition(_ position: UInt8) {
-    
-    /******** (1) CODE TO BE ADDED *******/
-    
-  }
-  
-  func sendBTServiceNotificationWithIsBluetoothConnected(_ isBluetoothConnected: Bool) {
-    let connectionDetails = ["isConnected": isBluetoothConnected]
-    NotificationCenter.default.post(name: Notification.Name(rawValue: BLEServiceChangedStatusNotification), object: self, userInfo: connectionDetails)
-  }
-  
+
+    // Mark: - CBPeripheralDelegate
+
+    func peripheral(_ peripheral: CBPeripheral, didDiscoverServices error: Error?) {
+        let uuidsForBTService: [CBUUID] = [PositionCharUUID]
+
+        if (peripheral != self.peripheral) {
+            // Wrong Peripheral
+            return
+        }
+
+        if (error != nil) {
+            return
+        }
+
+        if ((peripheral.services == nil) || (peripheral.services!.count == 0)) {
+            // No Services
+            return
+        }
+
+        for service in peripheral.services! {
+            if service.uuid == BLEServiceUUID {
+                peripheral.discoverCharacteristics(uuidsForBTService, for: service)
+            }
+        }
+    }
+
+    func peripheral(_ peripheral: CBPeripheral, didDiscoverCharacteristicsFor service: CBService, error: Error?) {
+        if (peripheral != self.peripheral) {
+            // Wrong Peripheral
+            return
+        }
+
+        if (error != nil) {
+            return
+        }
+
+        if let characteristics = service.characteristics {
+            for characteristic in characteristics {
+                if characteristic.uuid == PositionCharUUID {
+                    self.positionCharacteristic = (characteristic)
+                    peripheral.setNotifyValue(true, for: characteristic)
+
+                    // Send notification that Bluetooth is connected and all required characteristics are discovered
+                    self.sendBTServiceNotificationWithIsBluetoothConnected(true)
+                }
+            }
+        }
+    }
+
+    // Mark: - Private
+
+    func writePosition(_ position: UInt8) {
+
+        /******** (1) CODE TO BE ADDED *******/
+
+    }
+
+    func sendBTServiceNotificationWithIsBluetoothConnected(_ isBluetoothConnected: Bool) {
+        let connectionDetails = ["isConnected": isBluetoothConnected]
+        NotificationCenter.default.post(name: Notification.Name(rawValue: BLEServiceChangedStatusNotification), object: self, userInfo: connectionDetails)
+    }
 }
